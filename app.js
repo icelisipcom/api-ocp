6ed26eef645ee54a0b9b28eb549bc2c05a70d0ee
Isaac Celis Vargas <icelis@ipcom.com.mx>
2024-02-04 02:10:10 -0600

    http status

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola desde la API en Openshift!');
+// Endpoint que devuelve un código de estado 200 (Éxito)
+app.get('/success', (req, res) => {
+  res.status(200).send('OK - Código de estado 200');
+});
+
+// Endpoint que devuelve un código de estado 300 (Redirección)
+app.get('/redirect', (req, res) => {
+  res.redirect(301, 'https://www.ejemplo.com');
+});
+
+// Endpoint que devuelve un código de estado 400 (Solicitud incorrecta)
+app.get('/bad-request', (req, res) => {
+  res.status(400).send('Solicitud incorrecta - Código de estado 400');
+});
+
+// Endpoint que devuelve un código de estado 500 (Error interno del servidor)
+app.get('/internal-server-error', (req, res) => {
+  res.status(500).send('Error interno del servidor - Código de estado 500');
 });
 
 app.listen(port, () => {
-  console.log(`La API está escuchando en el puerto ${port}`);
+  console.log(`El API de estados está escuchando en el puerto ${port}`);
 });
-
